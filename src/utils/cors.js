8163b309a2de2b0028b0f090cba6549c94c406e9
MikeManzi <manzimike378@gmail.com>
2022-05-18 19:25:39 +0200

    chore(ESlint): configure ESlint

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,12 +1,12 @@
 exports.corsFunction = (req, res, next) => {
-  const allowedOrigins = ["https://isiteyves.github.io"];
-  const origin = req.headers.origin;
+  const allowedOrigins = ['https://isiteyves.github.io'];
+  const { origin } = req.headers;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
   next();
 };
