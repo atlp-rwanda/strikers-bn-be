690062d8faafdd7f8315f123d2a03b08a080c8eb
Kellia U <kellumuhire@gmail.com>
2022-06-06 18:31:05 +0200

    update(request): add verifyManager middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-exports.verifyToken = verifyToken = (req, res, next) => {
+exports.verifyToken = (req, res, next) => {
   try {
     let token = req.headers.authorization.split(" ")[1];
     jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
@@ -8,9 +8,17 @@ exports.verifyToken = verifyToken = (req, res, next) => {
         return res.status(401).send({ message: err.message });
       }
       req.userId = decoded.id;
+      req.role = decoded.roleId;
       next();
     });
   } catch {
     return res.status(403).send({ message: "No token provided!" });
   }
+};
+
+exports.verifyManager = (req, res, next) => {
+  if (req.roleId !== process.env.MANAGER_ID) {
+    return res.status(403).send({ message: "Not authorized." });
+  }
+  next();
 };
