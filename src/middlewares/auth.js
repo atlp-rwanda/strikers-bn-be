2b0f380573769cf6563ce0861a1da945dbad99ef
Yves Isite <61538146+IsiteYves@users.noreply.github.com>
2022-06-08 21:32:33 +0200

    Chore/#182215585 user role settings (#39)
    * [fix]: changed route naming convention
    
    * [feat] Added api to update the title(name) of an existing role
    
    * [fix]: fixed user role middlewares
    
    * [fix]: resolving changes
    
    * [fix]: fixed auth middlewares
    
    * [feat] Added all roles to the database
    
    * [fix]: fixed auth middlewares
    
    * [feat] Run the latest coverage for chore/#182215585-user-role-settings branch
    
    * [feat] Run tests for POST and PATCH endpoints of roles
    
    Co-authored-by: abi-seth <abiheloaf@gmail.com>
    Co-authored-by: Abi-Seth <75297615+Abi-Seth@users.noreply.github.com>

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken';
+import { TOKEN_SECRET } from "../config/key";
 
 export async function verifyToken (req, res, next) {
   try {
     let token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+    jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).send({ message: err.message });
       }
