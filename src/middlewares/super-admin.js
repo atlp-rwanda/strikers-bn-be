c850a560557e9398465653f4b8e3c373e0e9867f
abi-seth <abiheloaf@gmail.com>
2022-06-08 11:48:09 +0200

    [fix]: fixed user role middlewares

--- src/middlewares/super-admin.js
+++ src/middlewares/super-admin.js
@@ -1,5 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { TOKEN_SECRET } from "../config/key";
+import { Roles, User } from '../models';
+
 export async function adminCheck(req, res, next) {
-  if (req.user.role != "ADMIN")
-    return res.status(403).send("This route can be accessed by a super administrator only!");
-  next();
-}
+  try {
+    let token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, TOKEN_SECRET, async (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: err.message });
+      }
+      const user = await User.findOne({ where: { uuid: decoded.uuid } });
+      if (!user)
+        return res.status(404).send({ message: 'Invalid token owner' });
+      const role = await Roles.findOne({ where: { roleId: user.roleId } });
+      if (!role)
+        return res.status(404).send({ message: 'Invalid role' });
+      if (role.roleTitle != 'ADMIN')
+        return res.status(401).send({ message: 'This route can be accessed by a super administrator only!' });
+      next();
+    });
+  } catch {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+};
