4adeec3e3c9f1ef440209f49847c85dbbc6768a3
Nyituriki <igihozocolombe2003@gmail.com>
2022-06-13 21:31:48 +0200

    Resolve conflicts with user-role-settings

--- test/unit/role.test.js
+++ test/unit/role.test.js
@@ -12,15 +12,15 @@ describe("Role API", () => {
   /**
    * Test GET route
    */
-  describe("GET /api/roles", () => {
+  describe("GET /api/v1/roles", () => {
     it("It should GET a list of all roles", async () => {
-      const res = await chai.request(server).get("/api/roles");
+      const res = await chai.request(server).get("/api/v1/roles");
       expect(res).to.have.status(200);
       expect(res.body).to.be.a("array");
     });
 
     it("It should NOT GET a list of all roles", async () => {
-      const res = await chai.request(server).get("/api/role/all");
+      const res = await chai.request(server).get("/api/v1/role/all");
       expect(res).to.have.status(404);
     });
   });
@@ -28,23 +28,23 @@ describe("Role API", () => {
   /**
    * Test GET route for specific role
    */
-  describe("GET /api/roles/:id", () => {
+  describe("GET /api/v1/roles/:id", () => {
     it("It should GET a specific role by its specific id", async () => {
-      const id = "96c97445-d152-4a4e-9868-bee9d5a18ce2",
-        res = await chai.request(server).get("/api/roles/" + id);
+      const id = "c1f1d2bf-33bd-4e11-9d7a-0331db465f95",
+        res = await chai.request(server).get("/api/v1/roles/" + id);
       expect(res).to.have.status(200);
       expect(res.body).to.be.a("object");
     });
 
     it("It should NOT GET a specific role by its specific id (Non-existing role)", async () => {
       const id = "96c97445-d152-4a4e-9868-bee9d5a18ca2",
-        res = await chai.request(server).get("/api/roles/" + id);
+        res = await chai.request(server).get("/api/v1/roles/" + id);
       expect(res).to.have.status(404);
     });
 
     it("It should NOT GET a specific role by its specific id (Invalid UUID)", async () => {
       const id = "96c97445",
-        res = await chai.request(server).get("/api/roles/" + id);
+        res = await chai.request(server).get("/api/v1/roles/" + id);
       expect(res).to.have.status(500);
     });
   });
