cdd7d9138678ff5b2952bcfb74af2588f80653c8
NezaBruce <75426019+NezaBruce@users.noreply.github.com>
2022-06-16 10:39:09 +0200

    Ch/#182215588 logout functionality (#48)
    * fixed
    
    * Update README.md
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * feat(logout):Implementing-user-logout-functionality
    
    * update confyml
    
    * updateconfyml
    
    * updateconfyml
    
    * updateconfyml
    
    * feat(logout):Implementing-user-logout-functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,12 @@
-import { Router } from "express";
-import { verifyToken } from "../middlewares/auth";
+/* eslint-disable import/no-import-module-exports */
+import { Router } from 'express';
+import { verifyToken } from '../middlewares/auth';
+
 const userRouter = Router();
 
-const { addUser, signIn,editUser,getUsers,getUser,verifyUser } = require("../controllers/user.controller");
+const {
+  addUser, signIn, editUser, getUsers, getUser, verifyUser, logout
+} = require('../controllers/user.controller');
 
 /**
  * @description To create a new user
@@ -10,7 +14,7 @@ const { addUser, signIn,editUser,getUsers,getUser,verifyUser } = require("../con
  * @access Public
  * @type POST
  */
-userRouter.post("/register", addUser);
+userRouter.post('/register', addUser);
 
 /**
  * @description To update a user
@@ -18,16 +22,16 @@ userRouter.post("/register", addUser);
  * @access Public
  * @type PUT
  */
- userRouter.put("/updateuser/:uuid",verifyToken, editUser);
- userRouter.get("/getusers",getUsers);
- userRouter.get("/:uuid",getUser);
+userRouter.put('/updateuser/:uuid', verifyToken, editUser);
+userRouter.get('/getusers', getUsers);
+userRouter.get('/:uuid', getUser);
 /**
  * @description To login using email and password
  * @api v1/api/users/login
  * @access Public
  * @type POST
  */
-userRouter.post("/login", signIn);
+userRouter.post('/login', signIn);
 
 /**
  * @description To verify user account
@@ -35,6 +39,17 @@ userRouter.post("/login", signIn);
  * @access Public
  * @type GET
  */
-userRouter.get("/verify/:email", verifyUser);
+userRouter.get('/verify/:email', verifyUser);
+/**
+ * @description User Logout
+ * @api v1/api/users/:uuid/logout
+ * @access Public
+ * @type GET
+ */
+userRouter.get('/:uuid/logout', logout);
 
-module.exports = userRouter;
+userRouter.get('/:uuid/welcome',verifyToken,(req,res)=>{
+  console.log(req.params.uuid);
+  res.send(req.params.uuid+'walkverese');
+})
+export default userRouter;
