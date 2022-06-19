cd3286fc5698da29a2c1f271b1ea0374384149b2
IsiteYves <yvesisite@gmail.com>
2022-06-19 12:31:37 +0200

    [feat] Run coverage for all existing endpoints

--- test/unit/booking.test.js
+++ test/unit/booking.test.js
@@ -14,17 +14,31 @@ describe("Booking API", () => {
    */
   describe("GET /api/v1/bookings", () => {
     it("It should GET a list of all bookings", async () => {
-      const res = await chai.request(server).get("/api/v1/bookings");
+      const { body } = await chai
+          .request(server)
+          .post("/api/v1/users/login")
+          .send({ email: "regiskayitare@gmail.com", password: "pass12345" }),
+        res = await chai
+          .request(server)
+          .get("/api/v1/bookings")
+          .set({ authorization: `Bearer ${body.token}` });
       expect(res).to.have.status(200);
       expect(res.body).to.be.a("array");
     });
 
-    it("It should NOT GET a list of all bookings", async () => {
-      const res = await chai.request(server).get("/api/v1/bookings/all");
+    it("It should NOT GET a list of all bookings (Invalid extra info on route)", async () => {
+      const { body } = await chai
+          .request(server)
+          .post("/api/v1/users/login")
+          .send({ email: "regiskayitare@gmail.com", password: "pass12345" }),
+        res = await chai
+          .request(server)
+          .get("/api/v1/bookings/all")
+          .set({ authorization: `Bearer ${body.token}` });
       expect(res).to.have.status(500);
     });
 
-    it("It should NOT GET a list of all bookings", async () => {
+    it("It should NOT GET a list of all bookings (Unexisting route)", async () => {
       const res = await chai.request(server).get("/api/v1/booking/all");
       expect(res).to.have.status(404);
     });
