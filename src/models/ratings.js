8b843b6e9244c9d5a9340eac9688532ffbb63efb
pmuhire2002 <pmuhire2002@gmail.com>
2022-06-22 20:49:35 +0200

    ft(rating a centre): Done with the feature

--- src/models/ratings.js
+++ src/models/ratings.js
@@ -12,11 +12,19 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+    toJSON() {
+      return { ...this.get() };
+    }
   }
   Ratings.init({
-    userId: {
-      type: DataTypes.UUID,
+    id: {
       allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      type: DataTypes.UUID
     },
     accomodationId: {
       type: DataTypes.UUID,
