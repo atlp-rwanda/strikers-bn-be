62df8ab5463d901bd966d4ee37b82054a991bbd6
grace.fidele <gngrace10@gmail.com>
2022-07-19 13:44:57 +0200

    feat: Login in with Google oauth

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,22 @@
 /* eslint-disable import/no-import-module-exports */
-import { Router } from 'express';
-import { verifyToken } from '../middlewares/auth';
-
+import { Router } from "express";
+import { verifyToken } from "../middlewares/auth";
+import auth from "../utils/google-auth";
+import passport from "passport";
 const userRouter = Router();
 
 const {
-  addUser, signIn, editUser, getUsers, getUser, verifyUser, logout, resetPassword, newPassword
-} = require('../controllers/user.controller');
+  addUser,
+  signIn,
+  editUser,
+  getUsers,
+  getUser,
+  verifyUser,
+  logout,
+  resetPassword,
+  newPassword,
+  googleAuth,
+} = require("../controllers/user.controller");
 
 /**
  * @description To create a new user
@@ -14,7 +24,23 @@ const {
  * @access Public
  * @type POST
  */
-userRouter.post('/register', addUser);
+userRouter.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+userRouter.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/api/v1/users/getusers",
+    failureRedirect: "/auth/googleloginfailure",
+  })
+);
+
+userRouter.get("/auth/googleLoginFailure", (req, res) => {
+  res.send("Something went wrong..");
+});
+userRouter.post("/register", addUser);
 
 /**
  * @description To update a user
@@ -22,19 +48,19 @@ userRouter.post('/register', addUser);
  * @access Public
  * @type PUT
  */
-userRouter.put('/updateuser/:uuid', verifyToken, editUser);
-userRouter.get('/getusers', getUsers);
-userRouter.get('/:uuid', getUser);
+userRouter.put("/updateuser/:uuid", verifyToken, editUser);
+userRouter.get("/getusers", getUsers);
+userRouter.get("/:uuid", getUser);
 /**
  * @description To login using email and password
  * @api v1/api/users/login
  * @access Public
  * @type POST
  */
-userRouter.post('/login', signIn);
+userRouter.post("/login", signIn);
 
-userRouter.post('/resetpassword', resetPassword);
-userRouter.patch('/resetpassword', newPassword);
+userRouter.post("/resetpassword", resetPassword);
+userRouter.patch("/resetpassword", newPassword);
 
 /**
  * @description To verify user account
@@ -42,16 +68,16 @@ userRouter.patch('/resetpassword', newPassword);
  * @access Public
  * @type GET
  */
-userRouter.get('/verify/:email', verifyUser);
+userRouter.get("/verify/:email", verifyUser);
 /**
  * @description User Logout
  * @api v1/api/users/:uuid/logout
  * @access Public
  * @type GET
  */
-userRouter.get('/:uuid/logout', logout);
+userRouter.get("/:uuid/logout", logout);
 
-userRouter.get('/:uuid/welcome', verifyToken, (req, res) => {
+userRouter.get("/:uuid/welcome", verifyToken, (req, res) => {
   console.log(req.params.uuid);
   res.send(`${req.params.uuid}walkverese`);
 });
