85861832693cdc28bf646985ae9fb5df5738610c
acele-happy <78531760+acele-happy@users.noreply.github.com>
2022-07-28 21:31:15 +0200

    changed model name

--- src/models/notification.js
+++ src/models/notification.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class notifications extends Model {
+  class notification extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  notifications.init({
+  notification.init({
     uuid:{
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV$
@@ -40,7 +40,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    modelName: 'notifications',
+    modelName: 'notification',
   });
-  return notifications;
-}
+  return notification;
+};
--- test/unit/accommodation.test.js
+++ test/unit/accommodation.test.js
@@ -14,21 +14,13 @@
 //    */
 //   describe("GET /api/v1/accommodations", () => {
 //     it("It should GET  all accommodation", async () => {
-<<<<<<< HEAD
-//       const res = await chai.request(server).get("/api/v1/accommodations");
-=======
 //       const res = await chai.request(server).get("/api/v1/accommodations").set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiNzIxMTdhNDYtN2JhMi00OTVkLTg4NDYtMjIxMzEzNDcwYWQ0IiwiZW1haWwiOiJhYmloZWxvYWZAZ21haWwuY29tIiwicm9sZUlkIjoiYTc3YTUwYzItODlkMy00N2ZmLThkMjgtNDljMGMxN2VjOGE2IiwiaWF0IjoxNjU4MTgxMjk2fQ.nV7T-6r9sh0UUbu-LRFmW3zF8Pr1rbgRFdOZLQ6cWHE");
->>>>>>> 2f4cfcaeb59d53a8d820420d0d7a70e9b3bb75c6
 //       expect(res).to.have.status(200);
 //       expect(res.body).to.be.a("object")
 //     });
 
 //     it("It should NOT GET a list of all accommodation", async () => {
-<<<<<<< HEAD
-//       const res = await chai.request(server).get("/api/v1/accommodations");
-=======
 //       const res = await chai.request(server).get("/api/v1/accommodations").set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiNzIxMTdhNDYtN2JhMi00OTVkLTg4NDYtMjIxMzEzNDcwYWQ0IiwiZW1haWwiOiJhYmloZWxvYWZAZ21haWwuY29tIiwicm9sZUlkIjoiYTc3YTUwYzItODlkMy00N2ZmLThkMjgtNDljMGMxN2VjOGE2IiwiaWF0IjoxNjU4MTgxMjk2fQ.nV7T-6r9sh0UUbu-LRFmW3zF8Pr1rbgRFdOZLQ6cWHE");
->>>>>>> 2f4cfcaeb59d53a8d820420d0d7a70e9b3bb75c6
 //       expect(res).to.have.status(404);
 //     });
 //   });
