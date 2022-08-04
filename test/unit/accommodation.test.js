85ebe1c207292962b5290ce6097a720befc21705
Muhire Patrick <pmuhire2002@gmail.com>
2022-08-04 09:00:27 +0200

    ft: Done with tests(rating a center)

--- test/unit/accommodation.test.js
+++ test/unit/accommodation.test.js
@@ -35,7 +35,7 @@ describe("POST /api/v1/accommodations",() => {
     /**
      * Should rate an accomodation
      */
-     it("It should rate a given accomodation", () => {
+     it("It should not rate a given accomodation", () => {
       const id="b5e423ee-2e20-4d71-abbb-8924b414f778";
       const newRating={
         userId: "86f329b4-ab3f-4cd3-bea3-a527745fbcfb",
@@ -58,12 +58,37 @@ describe("POST /api/v1/accommodations",() => {
           // done();
         });
     });
+    it("It should rate a given accomodation", () => {
+      const id="b5e423ee-2e20-4d71-abbb-8924b414f778";
+      const newRating={
+        userId: "86f329b4-ab3f-4cd3-bea3-a527745fbcfc",
+        accomodationId: id,
+        numRating: 5
+      }
+      chai
+        .request(server)
+        .post(`/api/v1/accommodations/${id}/rate`)
+        .send(newRating)
+        .set({ authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiODZmMzI5YjQtYWIzZi00Y2QzLWJlYTMtYTUyNzc0NWZiY2ZiIiwiZW1haWwiOiJhYmlfc2V0aEBnbWFpbC5jb20iLCJyb2xlSWQiOiIwMTQxYzFhZS00MmExLTRiM2YtOWUyZC1iNzM2OWU5ZmY3ZDYiLCJpYXQiOjE2NTg5MTM0MzV9.2gP8niTuPL7Aw8MfGmo0crFg5frkxJwHtkrXl-SZ8xs" })
+        .end((err, response) => {
+          response.should.have.status(404);
+          response.should.be.a("object");
+          response.body.should.have
+            .property("message")
+            .eq("User not found! First register");
+          // done();
+        });
+    });
     it("It should not rate a given accomodation", () => {
       const id="d3a3ebae-b13b-42e9-afa8-319736274c02";
       chai
         .request(server)
         .post(`/api/v1/accommodations/${id}/rate`)
         .end((err, response) => {
+          response.should.be.a("object");
+          response.body.should.have
+            .property("message")
+            .eq("No token provided!");
           response.should.have.status(403);
         //   done();
         });
