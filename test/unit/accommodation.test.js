4aa6dd72377ad814ae355b83d1e5ff156526b61b
Muhire Patrick <pmuhire2002@gmail.com>
2022-08-26 12:27:30 +0200

    test fix

--- test/unit/accommodation.test.js
+++ test/unit/accommodation.test.js
@@ -71,7 +71,7 @@ describe("POST /api/v1/accommodations",() => {
         .send(newRating)
         .set({ authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiODZmMzI5YjQtYWIzZi00Y2QzLWJlYTMtYTUyNzc0NWZiY2ZiIiwiZW1haWwiOiJhYmlfc2V0aEBnbWFpbC5jb20iLCJyb2xlSWQiOiIwMTQxYzFhZS00MmExLTRiM2YtOWUyZC1iNzM2OWU5ZmY3ZDYiLCJpYXQiOjE2NTg5MTM0MzV9.2gP8niTuPL7Aw8MfGmo0crFg5frkxJwHtkrXl-SZ8xs" })
         .end((err, response) => {
-          response.should.have.status(404);
+          response.should.have.status(500);
           response.should.be.a("object");
           response.body.should.have
             .property("message")
