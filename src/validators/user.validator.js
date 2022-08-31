9120216cb5e0ce97e95c372cda9356ed7408f796
acele-happy <78531760+acele-happy@users.noreply.github.com>
2022-08-31 17:25:57 +0200

    changes

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -6,13 +6,11 @@ exports.validateUserRegisteration = (body) => {
     firstName: Joi.string().max(100).min(2).required(),
     lastName: Joi.string().max(100).min(2).required(),
     email: Joi.string().email().min(5).required(),
-    roleId: Joi.string().required(),
     phoneNumber: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
     password: Joi.string().min(8).required(),
-    lineManager: Joi.string().max(100).min(20).required()
   });
   return validUserRegisterSchema.validate(body);
-};
+}; 
 
 exports.validateUserAuthenatication = (body) => {
   const validUserLoginSchema = Joi.object({
